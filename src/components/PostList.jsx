7772342fad913746d7eb8b5d9c6d3d5f98cef390
nishasinghItis <nishasinghxyz341@gmail.com>
2025-02-22 16:23:34 +0530

    used useEffect for direct rendering of random posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,21 +1,23 @@
 import Post from "./Post.jsx"
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { PostList as PostListData} from "../store/Post-list-store.jsx";
 import WelcomeMsg from "./WelcomeMsg.jsx";
 
 const Postlist=()=>{
     const {postList, addInitialPosts} = useContext(PostListData);
-
-     const handleGetPostsClick =()=>{
-        fetch('https://dummyjson.com/posts')
-        .then(res => res.json())
-        .then((data) =>{
-            addInitialPosts(data.posts);
-        });
-    };
+     
+    useEffect(() =>{
+            fetch('https://dummyjson.com/posts')
+            .then(res => res.json())
+            .then((data) =>{
+                addInitialPosts(data.posts);
+            
+    },[]);
+});
+   
     return(
         <>
-        {postList.length === 0 && (<WelcomeMsg onGetPostsClick={handleGetPostsClick} />)}
+        {postList.length === 0 && (<WelcomeMsg />)}
        {postList.map((post)=>
        (<Post key={post.id} post={post}/>)
        )}
--- src/components/WelcomeMsg.jsx
+++ src/components/WelcomeMsg.jsx
@@ -2,7 +2,6 @@ const WelcomeMsg= ({onGetPostsClick})=>{
     return <>
     <center className="welcomeMsg">
     <h1 >There are no post yet</h1>
-    <button type="button" className="btn btn-primary" onClick={onGetPostsClick}>Fetch post from server</button>
     </center>
     </>
 }
