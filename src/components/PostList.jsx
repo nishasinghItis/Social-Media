8c3c7469bc33b2144466e5eb776028891237ee5e
nishasinghItis <nishasinghxyz341@gmail.com>
2025-02-22 18:29:29 +0530

    added a spinner for loading

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,24 +1,31 @@
 import Post from "./Post.jsx"
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PostList as PostListData} from "../store/Post-list-store.jsx";
 import WelcomeMsg from "./WelcomeMsg.jsx";
+import LoadingSpinner from "./LoadingSpinner.jsx";
 
 const Postlist=()=>{
     const {postList, addInitialPosts} = useContext(PostListData);
-     
+     const [fetching, setFetching] =useState(false);
+
     useEffect(() =>{
+        setFetching(true);
+        console.log("fetch srarted");
             fetch('https://dummyjson.com/posts')
             .then(res => res.json())
             .then((data) =>{
                 addInitialPosts(data.posts);
-            
-    },[]);
-});
+                setFetching(false);
+                console.log('fetch returnedd')            
+    });
+    console.log('fetch ended');
+},[]);
    
     return(
         <>
-        {postList.length === 0 && (<WelcomeMsg />)}
-       {postList.map((post)=>
+        {fetching && <LoadingSpinner/>}
+       {!fetching && postList.length === 0 && (<WelcomeMsg />)}
+       {!fetching && postList.map((post)=>
        (<Post key={post.id} post={post}/>)
        )}
         </>
